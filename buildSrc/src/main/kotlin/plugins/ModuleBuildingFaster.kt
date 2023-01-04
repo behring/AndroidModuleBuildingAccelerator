0edc63e032a954ef36d59561605ff9295b9fd470
behring <behring.zhao@gmail.com>
2023-01-04 21:35:51 +0800

    adjust dependency between modules

--- buildSrc/src/main/kotlin/plugins/ModuleBuildingFaster.kt
+++ buildSrc/src/main/kotlin/plugins/ModuleBuildingFaster.kt
@@ -2,12 +2,14 @@ package plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.get
 
 class ModuleBuildingFaster : Plugin<Project> {
     override fun apply(target: Project) {
         target.task("cacheArtifacts") {
             doLast {
                 println("hello zhaolin")
+                val implementation = target.configurations.getByName("implementation")
             }
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,11 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "MultiModuleBuildFaster"
-include ':app:sg'
-include ':app:hk'
-include ':feature:payments'
-include ':feature:home'
-include ':infra:network'
-include ':infra:analytics'
+include(":app")
+include(":feature:payments")
+include(":feature:home")
+include(":infra:network")
+include(":infra:analytics")
+
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
